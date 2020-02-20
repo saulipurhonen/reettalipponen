3a4c222ea2fbd87691d52d1fb13194d1ecbb5dfd
saulipurhonen <sauli.purhonen@gmail.com>
2020-02-20 23:23:03 +0200

    Theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#3cb371`,
-        theme_color: `#3cb371`,
+        background_color: `#a3a2b0`,
+        theme_color: `#a3a2b0`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
