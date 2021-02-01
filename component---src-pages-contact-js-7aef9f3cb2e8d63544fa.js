7bb01ec3aa378118c12b7b5f8cb5d00dda05be6b
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-02-01 21:28:56 +0200

    Updates

---
+++ component---src-pages-contact-js-7aef9f3cb2e8d63544fa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Yhteystiedot"}),l.a.createElement("h1",null,"Yhteystiedot"),l.a.createElement("p",null,"reetta.lipponen@hotmail.com"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-7aef9f3cb2e8d63544fa.js.map
