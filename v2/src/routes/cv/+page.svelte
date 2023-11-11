2ac24eb99cd884c3ea2358a578e77370260df57b
saulipurhonen <sauli.purhonen@gmail.com>
2023-11-11 11:22:04 +0200

    feat: CV content fetch & render

--- v2/src/routes/cv/+page.svelte
+++ v2/src/routes/cv/+page.svelte
@@ -1,12 +1,30 @@
+<script>
+	export let data;
+</script>
+
 <svelte:head>
 	<title>CV</title>
 	<meta name="description" content="CV" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>CV</h1>
+<header>
+	<h1 class="text-6xl md:text-8xl font-bold tracking-tighter leading-tight text-slate-50 text-left">
+		CV
+	</h1>
+</header>
 
-	<p>
-		TODO
-	</p>
+<div class="container mt-12">
+	{#each data.sections as section}
+		<div class="pb-12">
+			<h2 class="text-2xl font-bold py-3 text-slate-50">{section.heading}</h2>
+			{#each section.entries as entry}
+				<div class="grid grid-flow-row-dense grid-cols-6 gap-1 py-3 sm:py-1">
+					{#if entry.date}
+						<div class="text-slate-20">{entry.date}</div>
+					{/if}
+					<div class="col-span-6 sm:col-span-5 text-slate-50">{entry.content}</div>
+				</div>
+			{/each}
+		</div>
+	{/each}
 </div>
