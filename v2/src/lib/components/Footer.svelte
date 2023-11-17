a2663f3adb7b36a6c13ad541aeccfcc0e77276a9
saulipurhonen <sauli.purhonen@gmail.com>
2023-11-17 21:19:12 +0200

    feat: Footer component

---
+++ v2/src/lib/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+  const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+  <p>© {currentYear} Reetta Lipponen</p>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
