beacec51907f593790b233edfcd93864d5b0187f
saulipurhonen <sauli.purhonen@gmail.com>
2023-11-18 21:57:41 +0200

    Page header & content fade-in

--- v2/src/lib/components/PageContent.svelte
+++ v2/src/lib/components/PageContent.svelte
@@ -1,4 +1,24 @@
-<section class="container py-20">
+<script>
+    import { gsap } from 'gsap';
+
+    /**
+   * @type HTMLElement
+   */
+    let contentContainer;
+
+    $: if (contentContainer) {
+    gsap.set(contentContainer, { opacity: 0 });
+    contentContainer.classList.remove('invisible');
+    gsap.to(contentContainer, {
+      opacity: 1,
+      duration: 1.5,
+      ease: 'power2.inOut',
+    });
+  }
+
+</script>
+
+<section bind:this={contentContainer} class="container py-20 invisible">
   <div class="max-w-xl backdrop-blur-sm">
     <slot />
   </div>
