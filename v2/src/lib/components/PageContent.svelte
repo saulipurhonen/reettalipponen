d1f94ba8af5ad9ee6e9e8137926052dc84b7d760
saulipurhonen <sauli.purhonen@gmail.com>
2024-01-06 09:38:36 +0200

    Workaround for slug page animation reset

--- v2/src/lib/components/PageContent.svelte
+++ v2/src/lib/components/PageContent.svelte
@@ -1,12 +1,16 @@
 <script>
+  import { page } from '$app/stores';
   import { gsap } from 'gsap';
 
   /**
    * @type HTMLElement
    */
   let contentContainer;
-
-  $: if (contentContainer) {
+  $: currentPath = $page?.url?.pathname;
+  // TODO: Restart animation when in slug route change
+  // Current path check is a workaround for this for now.
+  // Better approach would be resetting the animation when slug changes.
+  $: if (contentContainer && currentPath) {
     contentContainer.classList.remove('invisible');
 
     const paragraphs = document.querySelectorAll('.reveal');
