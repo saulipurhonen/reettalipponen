f9024bb25bcbcf090eec06c9b7697b3852bfd370
saulipurhonen <48789543+saulipurhonen@users.noreply.github.com>
2026-02-13 22:35:48 +0200

    Remove tina generated files from tracking

--- v3/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '9ad050e9e8cde08d2bd7ed9e3a1022dd1d644dd9', queries,  });
-export default client;
-  
