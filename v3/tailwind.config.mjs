4f6935d339a01ccb71472134660c16df0458e04d
saulipurhonen <48789543+saulipurhonen@users.noreply.github.com>
2026-02-15 22:00:08 +0200

    fip: font style

--- v3/tailwind.config.mjs
+++ v3/tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        display: ['Caveat', 'cursive'],
+      },
       colors: {
         surface: {
           DEFAULT: '#0a0f0a',
