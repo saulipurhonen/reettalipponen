b87e685af5b6611a06c4deb7c0d8acbe2856f4ec
saulipurhonen <48789543+saulipurhonen@users.noreply.github.com>
2026-02-16 21:39:33 +0200

    wip: untrack generated

--- v3/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'c1193c7e6e944da1ce868fd0c798bdcd07ad97f4', queries,  });
-export default client;
-  
